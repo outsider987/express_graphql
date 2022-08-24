0861e1f4f6b08aed992aa26846e599b8077213a7
Outsider <t7902195204@gmail.com>
2022-08-24 21:31:04 +0800

    :add:add dist git ignore

--- dist/graphql/resolvers/Query/token.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const token_1 = require("../../../models/token");
-const tokenResolver = () => {
-    return token_1.Token.findAll();
-};
-exports.default = tokenResolver;

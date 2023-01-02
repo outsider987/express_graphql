32323f04986e833b908c520b38bec535c52f71d7
victor_chang <victor_chang@mediconcen.com>
2023-01-02 18:34:37 +0800

    post get api

--- src/http/api/posts/service.ts
+++ src/http/api/posts/service.ts
@@ -1,9 +1,12 @@
 import { Request, Response } from 'express';
+import { title } from 'process';
 import BaseService from '~/http/common/baseService';
 
 class PostsService extends BaseService {
-    async getPosts(res: Request) {
-        return this.prisma.post.findMany();
+    async getPosts(searchText: string) {
+        return this.prisma.post.findMany({
+            where: { title: { contains: searchText }, author: { OR: { name: { contains: searchText } } } },
+        });
     }
 }
 export default PostsService;
